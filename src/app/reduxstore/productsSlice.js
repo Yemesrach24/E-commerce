ce08551ce0e2c66c44795b77a3136d2c098ecd0c
Yemesrach24 <99786617+Yemesrach24@users.noreply.github.com>
2025-01-26 18:52:27 +0300

    Update productsSlice.js

--- src/app/reduxstore/productsSlice.js
+++ src/app/reduxstore/productsSlice.js
@@ -1,37 +1,49 @@
-import { createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = { items: [], };
+
+const initialState = { items: [] };
 
 export const productsSlice = createSlice({
-    name: 'products-cart',
-    initialState,
-    reducers: {
-        addProduct: (state, action )  => {
+ 
+  name: 'products-cart', 
+  initialState,
+  reducers: {
+   
+    addProduct: (state, action) => {
+      const existingProduct = state.items.find(
+        (item) => item.id === action.payload.id
+      );
 
-            const existingProduct = state.items.find(
-                (item) => item.id === action.payload.id
-            );
-
-            if (existingProduct) {
-                existingProduct.quantity++;
-            } else { 
-                state.items.push({ ...action.payload, quantity: 1 });
+      if (existingProduct) {
+       existingProduct.quantity++;
+      } 
+       else {
+    
+        state.items.push({ ...action.payload, quantity: 1 });
             }
-        },
-
-        deleteProduct:(state, action ) =>{
-           
-            const existingProduct = state.items.find(
-                (item) => item.id === action.payload.id
-            );
-            if (existingProduct) {
-                existingProduct.quantity--;
-            }
-
-
-        }      
     },
+
+   
+    deleteProduct: (state, action) => {
+      const existingProduct = state.items.find(
+        (item) => item.id === action.payload.id
+      );
+      if (existingProduct && existingProduct.quantity > 1) {
+      
+        existingProduct.quantity--;
+      } else {
+      
+        state.items = state.items.filter((item) => item.id !== action.payload.id);
+      }
+    },
+  },
 });
+
+
+export const selectCartQuantity = (state) => {
+  return state['products-cart'].items.reduce((total, item) => total + item.quantity, 0);
+};
+
 
 export const { addProduct, deleteProduct } = productsSlice.actions;
 export default productsSlice.reducer;
