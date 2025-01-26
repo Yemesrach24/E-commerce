7d4d916f1704082899a2fb43a802efe7bd632057
Yemesrach24 <99786617+Yemesrach24@users.noreply.github.com>
2025-01-26 18:58:41 +0300

    Update Header.jsx

--- src/app/component/Header.jsx
+++ src/app/component/Header.jsx
@@ -1,27 +1,31 @@
+'use client';
 import React from 'react';
-//import { ShoppingCartIcon } from "@heroicons/react/24/outline";
-import Link from 'next/link';  
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import Link from 'next/link';
+import { useSelector } from 'react-redux';
+import { selectCartQuantity } from '@/app/reduxstore/productsSlice'; 
+
 const Header = () => {
+  const cartQuantity = useSelector(selectCartQuantity);
+
   return (
-    <header className='bg-slate-400 py-4 drop-shadow-md'>  
-      <div className='mx-auto  px-4 flex items-center justify-between '>
-
-        <div>
-          <span className='text-black font-bold'>E-<span className='text-white font-bold'>commerce</span></span>
+    <header className='bg-black py-4 drop-shadow-md fixed top-0 left-0 right-0 z-50 h-20'>
+      <div className='mx-auto px-4 flex items-center justify-between'>
+        <div className='text-2xl'>
+          <span className='text-yellow-500 font-bold'>E-<span className='text-yellow-200 font-bold'>commerce</span></span>
         </div>
 
-        <div className='hidden sm:flex space-x-10'>
-          <p>Home</p>
-          <p>About us</p>
-          <p>Latest</p>
-        </div>
-
-        <div>
-          {/* Use Link for navigation */}
+        <div className="relative mr-5">
           <Link href="/cart">
+            <ShoppingCartIcon className="h-10 w-10 text-yellow-500" />
+           
+            {cartQuantity > 0 && (
+              <div className="absolute top-0 right-0 rounded-full bg-yellow-200 text-black text-xs w-5 h-5 flex items-center justify-center">
+            {cartQuantity}
 
-              <ShoppingCartIcon className="h-6 w-6 text-gray-500" />
-        
+              </div>
+            )}
+
           </Link>
         </div>
 
